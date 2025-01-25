1cb91ae08cd57c097a82924d5a0a4b0a0c59f09e
Yasowant Nayak <129403851+Yasowant@users.noreply.github.com>
2025-01-25 11:24:06 +0000

    array and loops in java script

---
+++ 05_loops/one.js
@@ -0,0 +1,46 @@
+//for
+
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    if (element === 5) {
+        // console.log("5 is the best Number")
+    }
+    // console.log(element)
+
+}
+
+
+for (let i = 0; i < 10; i++) {
+    // console.log(`outer Loop :${i}`)
+    for (let j = 0; j <= 10; j++) {
+        // console.log(`inner Loop :${j} and ${i}`)
+        // console.log(i + '*' + j + '=' + i * j)
+    }
+}
+
+let myArray=['flash','batman','superman'];
+for(let i=0;i<myArray.length;i++){
+    const element=myArray[i];
+    console.log(element)
+}
+
+//break and continue
+// for (let index = 1; index <=20; index++) {
+//     if(index === 5){
+//         console.log(`detected ${index}`);
+//         break;
+        
+//     }
+//    console.log(`value of i is ${index}`)
+    
+// }
+
+
+for (let index = 1; index <=20; index++) {
+    if(index === 5){
+        console.log(`detected ${index}`);
+        continue;
+    }
+   console.log(`value of i is ${index}`)
+    
+}
